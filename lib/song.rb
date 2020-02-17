8244567c7dccbbdac93ff85e53c255a78a58d3cc
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-17 18:01:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,30 @@ class Song
     self.class.all << self
   end
 
+def self.create 
+  song = self.new 
+  self.all << song
+ song
+  end 
+ 
+ def self.new_by_name(title)
+   song = self.new 
+   song.name = title 
+   song
+  end 
+def self.create_by_name(title)
+    song = self.create 
+   song.name = title 
+   song
+ end
+ 
+ def self.find_by_name(title)
+   result = self.all.detect {|song| song.name == title}
+   result 
+
 end
+def self.find_or_create_by_name(title)
+  result = self.
+  
+  end
+ end
