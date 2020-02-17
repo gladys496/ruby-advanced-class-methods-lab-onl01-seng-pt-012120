906517434c6c68263287eb05d74a6c64a9fecf98
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-17 18:45:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ def self.find_or_create_by_name(title)
     song_array[1] = song_array[1].chomp(".mp3")
     song = self.new
     song.name = song_array[1]
-    song.artist.name = song_array[0]
+    song.artist_name = song_array[0]
     song
   end 
  end 
